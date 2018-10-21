19310af747dea1fb3df1073f2e383064b66e598c
NahoyGames <yohanishereyt@gmail.com>
2018-10-21 06:10:31 -0700

    Lego Bricks + Wrecking Ball

---
+++ Assets/Assets/Models/Environments/Bricks/BrickBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehavior : MonoBehaviour {
+
+    public Color[] brickColors;
+    
+	void Start () {
+
+        this.GetComponent<MeshRenderer>().material.color = brickColors[Random.Range(0, brickColors.Length - 1)];
+
+	}
+}
