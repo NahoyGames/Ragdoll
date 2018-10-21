940f97a7aa4fc1a46fea45194f4ab1e834ba066a
NahoyGames <yohanishereyt@gmail.com>
2018-10-21 08:20:03 -0700

    Slow-Mo Settings

---
+++ Assets/Recording.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recording : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 0.05f;
+            Cursor.visible = false;
+            this.transform.position = new Vector3(transform.position.x, transform.position.y, -20);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 1f;
+            Cursor.visible = true;
+            this.transform.position = new Vector3(transform.position.x, transform.position.y, -50);
+        }
+
+    }
+}
