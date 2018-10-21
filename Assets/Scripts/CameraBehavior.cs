a8a88814a903d4c7b3aa578557e7ad1b08e0d49e
NahoyGames <yohanishereyt@gmail.com>
2018-10-20 17:17:38 -0700

    Improvements to Grappling Hook
    - Now detects collision on the rope itself
    - Other misc. stuff

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public Transform target;
+
+    private Vector3 zero = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 targetPos = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), ref zero, 0.3f);
+        transform.position = new Vector3(targetPos.x, Mathf.Clamp(targetPos.y, 1, 100000), targetPos.z);
+    }
+}
--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -5,112 +5,95 @@ using UnityEngine;
 public class GrapplingHook : MonoBehaviour {
 
     [SerializeField] private GameObject hookPrefab;
-    [SerializeField] private GameObject nodePrefab;
+    [SerializeField] private Transform target;
 
     private GameObject hook;
+    private SpringJoint hookJoint;
+    private HasCollision hookCollision;
 
     private void Update()
     {
+        // TODO temporary hook fire input
         if (Input.GetButtonDown("Fire1"))
         {
-            if (hook != null)
+            if (HasHook)
             {
-                Destroy(hook);
+                DestroyHook();
             }
             else
             {
-                hook = Instantiate(hookPrefab, this.transform.GetChild(1).position, Quaternion.identity);
-                hook.GetComponent<Rigidbody>().velocity = (targetMouse.MouseWorldPos() - hook.transform.position).normalized * 50;
+                FireHook();
             }
         }
 
+        // Hook update
         if (hook != null)
         {
-            if (hook.GetComponent<SpringJoint>() == null && hook.GetComponent<HasCollision>().IsColliding)
-            {
-                SpringJoint joint = hook.AddComponent<SpringJoint>();
-
-                joint.connectedBody = this.GetComponent<Rigidbody>();
-                joint.autoConfigureConnectedAnchor = false;
-                joint.spring = 1000;
-
-                hook.GetComponent<Rigidbody>().isKinematic = true;
-            }
-            else if (hook.GetComponent<SpringJoint>() != null)
-            {
-                Vector3 anchor = hook.GetComponent<SpringJoint>().connectedAnchor;
-                hook.GetComponent<SpringJoint>().connectedAnchor = anchor.normalized * Mathf.Clamp((float)(anchor.magnitude - 0.2), 3, 1000);
-            }
-
-            hook.GetComponent<LineRenderer>().SetPositions(new Vector3[] {
-                this.transform.GetChild(1).position,
-                hook.transform.position
-            });
+            UpdateHook();
         }
-    }
-
-    /*private List<GameObject> nodes;
-    private bool hookGotCollision = false;
-
-    private void Update()
-    {
-        if (hook != null)
-        {
-            if (!hookGotCollision && nodes.Count < 200)
-            {
-                MakeNodes();
-            }
-            else if (hook.GetComponent<HasCollision>().IsColliding)
-            {
-                hookGotCollision = true;
-            }
-
-        }
-        else if (Input.GetButtonDown("Fire1"))
-        {
-            FireHook();
-        }
-        
     }
 
     public void FireHook()
     {
         hook = Instantiate(hookPrefab, this.transform.GetChild(1).position, Quaternion.identity);
-        hook.GetComponent<Rigidbody>().velocity = (targetMouse.MouseWorldPos() - hook.transform.position).normalized * 50;
-
-        nodes = new List<GameObject>
-        {
-            hook
-        };
+        hook.GetComponent<Rigidbody>().AddForce((targetMouse.MouseWorldPos() - hook.transform.position).normalized * 15, ForceMode.Impulse);
+        hookCollision = hook.GetComponent<HasCollision>();
     }
 
-    private void MakeNodes()
+    public void DestroyHook()
     {
-        GameObject lastNode = nodes[nodes.Count - 1];
-        GameObject currentNode = lastNode;
+        Destroy(hook);
+    }
 
-        // Get the number of nodes to create between the last node and the hook
-        int nodesToCreate = (int)(Vector3.Distance(lastNode.transform.position, this.transform.GetChild(1).position) / 0.5);
-
-        Debug.Log(nodesToCreate);
-
-        for (int i = 0; i < nodesToCreate; i++)
+    public bool HasHook
+    {
+        get
         {
-            SpringJoint lastNodeJoint = currentNode.AddComponent<SpringJoint>();
-
-            currentNode = Instantiate(nodePrefab, Vector3.Lerp(lastNode.transform.position, this.transform.GetChild(1).position, 1 / (i + 1)), Quaternion.identity);
-
-
-            // Set the joint's anchor
-            lastNodeJoint.autoConfigureConnectedAnchor = true;
-            lastNodeJoint.connectedBody = currentNode.GetComponent<Rigidbody>();
-            lastNodeJoint.autoConfigureConnectedAnchor = false;
-
-            // Joint settings
-            lastNodeJoint.spring = 100000;
-            lastNodeJoint.damper = 100000;
-
-            nodes.Add(currentNode);
+            return hook != null;
         }
-    }*/
+    }
+
+    private void FinishHook()
+    {
+        hookJoint = hook.AddComponent<SpringJoint>();
+
+        hookJoint.connectedBody = this.GetComponent<Rigidbody>();
+        hookJoint.autoConfigureConnectedAnchor = false;
+        hookJoint.spring = 1000;
+        hookJoint.damper = 100;
+
+        hook.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void UpdateHook()
+    {
+        if (hookJoint != null) // Pull the hook up
+        {
+            Vector3 anchor = hookJoint.connectedAnchor;
+            hookJoint.connectedAnchor = anchor.normalized * Mathf.Clamp((float)(anchor.magnitude - 0.5), 3, 1000);
+        }
+        else if (hookCollision.IsColliding) // Hook Collision
+        {
+            FinishHook();
+        }
+        else // Hook in-between Collision
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(target.position, hook.transform.position - target.position, out hit))
+            {
+                if (hit.collider.tag == "Environment")
+                {
+                    hook.transform.position = hit.point;
+
+                    FinishHook();
+                }
+            }
+        }
+
+        // Line Renderer
+        hook.GetComponent<LineRenderer>().SetPositions(new Vector3[] {
+                this.transform.GetChild(1).position,
+                hook.transform.position
+            });
+    }
 }
